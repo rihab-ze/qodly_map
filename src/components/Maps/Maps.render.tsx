9c755fa5e7eb0cff9ba77cbc42a685d0ea485652
Ayoub METWALLI <ayoub.metwalli@4d.com>
2024-05-14 19:52:23 +0100

    update code

--- src/components/Maps/Maps.render.tsx
+++ src/components/Maps/Maps.render.tsx
@@ -77,7 +77,7 @@ const Maps: FC<IMapsProps> = ({
 
   return (
     <div ref={connect} style={style} className={cn(className, classNames)}>
-      <div ref={ref} style={{ width: '100%', height: '100%', border: '1px solid black' }}>
+      <div ref={ref} style={{ width: '100%', height: '100%' }}>
         {markerTypes == 'multiple' ? (
           <MultipleMarker
             data={value1}
