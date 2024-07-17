48e5dbf9c88330bb6c64bcdfe32a42fe87ca7a1b
rihab-ze <rihabzekrioui5@gmail.com>
2024-07-17 10:23:41 +0100

    first commit

---
+++ src/components/Map/Map.render.tsx
@@ -0,0 +1,181 @@
+import { useRenderer, useSources } from '@ws-ui/webform-editor';
+import cn from 'classnames';
+import { FC, useEffect, useState, useRef } from 'react';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+import { IMapProps } from './Map.config';
+
+interface LoactionAndPopup {
+  longitude: number;
+  latitude: number;
+  popupMessage: HTMLElement | null;
+}
+
+const Map: FC<IMapProps> = ({
+  popup,
+  zoom,
+  markerDragging,
+  animation,
+  mapDragging,
+  marker,
+  long,
+  lat,
+  tooltiop,
+  style,
+  className,
+  classNames = [],
+}) => {
+  const { connect } = useRenderer();
+  const [value, setValue] = useState<LoactionAndPopup | undefined>(undefined);
+  const [size, setSize] = useState({ width: style?.width, height: style?.height });
+  const ref = useRef<HTMLElement | null>(null);
+  const {
+    sources: { datasource: ds },
+  } = useSources();
+
+  function getValueByPath(obj: any, path: string) {
+    let keys = path.split('.');
+    function traverse(obj: any, keys: any) {
+      if (keys.length === 0) {
+        return obj;
+      }
+      if (obj && typeof obj === 'object') {
+        let key = keys.shift();
+        return traverse(obj[key], keys);
+      } else {
+        return undefined;
+      }
+    }
+    return traverse(obj, keys);
+  }
+
+  useEffect(() => {
+    if (!ds) return;
+    const listener = async (/* event */) => {
+      const v = await ds.getValue();
+      if (getValueByPath(v, long) && getValueByPath(v, lat)) {
+        setValue({
+          longitude: +getValueByPath(v, long),
+          latitude: +getValueByPath(v, lat),
+          popupMessage: getValueByPath(v, tooltiop),
+        });
+      }
+    };
+    listener();
+    ds.addListener('changed', listener);
+    return () => {
+      ds.removeListener('changed', listener);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ds]);
+
+  const mapRef = useRef<HTMLDivElement>(null);
+  const map = useRef<L.Map | null>(null);
+  const markers = useRef<L.Marker | null>(null);
+  var defaultIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/rihab-ze/qodly_map/develop/public/marker-icon.png',
+    iconSize: [26, 42],
+    iconAnchor: [13, 43],
+    popupAnchor: [0, -36],
+  });
+  useEffect(() => {
+    if (mapRef.current) {
+      map.current = L.map(mapRef.current, { dragging: mapDragging }).setView(
+        [value!.latitude, value!.longitude],
+        zoom,
+      );
+      mapRef.current.addEventListener('mousedown', (event) => {
+        event.stopPropagation();
+      });
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors',
+      }).addTo(map.current);
+
+      if (marker) {
+        markers.current = L.marker([+value!.latitude, +value!.longitude], {
+          draggable: markerDragging,
+          icon: defaultIcon,
+        }).addTo(map.current);
+        if (popup) {
+          const popUpMessage = value!.popupMessage as HTMLElement;
+          markers.current.bindPopup(popUpMessage);
+        }
+        // Attach event listener to listen for map moveend
+        markers.current.on('moveend', (event) => {
+          const newCenter = (event.target as L.Marker).getLatLng();
+          setValue({
+            longitude: newCenter.lng,
+            latitude: newCenter.lat,
+            popupMessage: value!.popupMessage,
+          });
+        });
+        markers.current.on('mousedown', (event) => {
+          event.originalEvent?.stopPropagation(); // Stop the event bubbling
+        });
+      }
+    }
+    // cleanUP
+    return () => {
+      if (map) map.current?.remove();
+    };
+  }, [markerDragging, zoom, map, mapDragging, popup, value, size, tooltiop]);
+  useEffect(() => {
+    map.current?.flyTo([value!.latitude, value!.longitude], zoom, {
+      animate: animation,
+    });
+    if (map.current && marker) {
+      markers.current?.setLatLng({
+        lat: value!.latitude,
+        lng: value!.longitude,
+      });
+
+      if (popup) {
+        const popUpMessage = value!.popupMessage as HTMLElement;
+        markers.current?.bindPopup(popUpMessage);
+      }
+    }
+  }, [value]);
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const { width, height } = entry.contentRect;
+        setSize({ width, height });
+      }
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+  function isLocationAndPopup(obj: any): obj is LoactionAndPopup {
+    return typeof obj == 'object' && !Array.isArray(obj) && 'latitude' in obj && 'longitude' in obj;
+  }
+  return (
+    <div ref={connect} style={style} className={cn(className, classNames)}>
+      {isLocationAndPopup(value) ? (
+        <div ref={mapRef} style={size} />
+      ) : (
+        <div
+          className="bg-red-50 border border-red-300 text-red-700 px-4 py-3 rounded-lg shadow-md"
+          role="alert"
+        >
+          <div className="flex items-center">
+            <strong className="font-bold text-red-700">Error!</strong>
+          </div>
+          <span className="block sm:inline mt-1 ">
+            Datasource does not match the expected format.
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Map;
---
+++ src/components/Map/Map.settings.ts
@@ -0,0 +1,81 @@
+import { ESetting, TSetting } from '@ws-ui/webform-editor';
+import { BASIC_SETTINGS, DEFAULT_SETTINGS, load } from '@ws-ui/webform-editor';
+
+const commonSettings: TSetting[] = [
+  {
+    key: 'zoom',
+    label: 'Zoom',
+    type: ESetting.NUMBER_FIELD,
+    defaultValue: 10,
+  },
+  {
+    key: 'mapDragging',
+    label: 'Map dragging',
+    type: ESetting.CHECKBOX,
+    defaultValue: true,
+  },
+  {
+    key: 'animation',
+    label: 'Animation',
+    type: ESetting.CHECKBOX,
+    defaultValue: true,
+  },
+  {
+    key: 'marker',
+    label: 'Marker',
+    type: ESetting.CHECKBOX,
+    defaultValue: false,
+  },
+  {
+    key: 'popup',
+    label: 'Popup',
+    type: ESetting.CHECKBOX,
+    defaultValue: false,
+  },
+  {
+    key: 'markerDragging',
+    label: 'Single marker dragging',
+    type: ESetting.CHECKBOX,
+    defaultValue: false,
+  },
+  {
+    key: 'long',
+    label: 'Longitude',
+    type: ESetting.TEXT_FIELD,
+  },
+  {
+    key: 'lat',
+    label: 'Latitude',
+    type: ESetting.TEXT_FIELD,
+  },
+  {
+    key: 'tooltiop',
+    label: 'Tooltip',
+    type: ESetting.TEXT_FIELD,
+  },
+];
+
+const Settings: TSetting[] = [
+  {
+    key: 'properties',
+    label: 'Properties',
+    type: ESetting.GROUP,
+    components: commonSettings,
+  },
+  ...load(DEFAULT_SETTINGS).filter('style.overflow', 'font', 'background'),
+];
+
+export const BasicSettings: TSetting[] = [
+  ...commonSettings,
+  ...load(BASIC_SETTINGS).filter(
+    'style.overflow',
+    'style.fontFamily',
+    'style.fontWeight',
+    'style.fontSize',
+    'style.textAlign',
+    'style.textTransform',
+    'background',
+  ),
+];
+
+export default Settings;
